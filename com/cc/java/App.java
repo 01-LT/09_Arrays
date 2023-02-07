a6291b7be0cfa1786f935772b7158b2a06ebebe5
Lawrence <lawrence.langen@gmail.com>
2023-02-07 11:47:14 +0100

    Korrektur

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -6,11 +6,11 @@ public class App {
 
     public static void main(String[] args) {
 
-      	// // Deklaration: int
+      	// Deklaration: int
 		// int [] intArr = new int [4];
 		// output("Länge Array: " + intArr.length);	
 		
-		// // Index
+		// // // Index
 		// output("3. Element " + intArr[2]);
 		
 		// intArr[0] = 10;
@@ -23,9 +23,9 @@ public class App {
 		// 	output("Element: " + i + " : "+ intArr[i]);
 		// }
 			
-		// Deklaration: String
-		// String [] strArr = new String [2];
-		// String [] strArr = {"Hallo","Nikola"};
+		// // Deklaration: String
+		// // String [] strArr = new String [2];
+		// String [] strArr = {"Hallo","Patrick"};
 		
 		// for (int i = 0; i < strArr.length; i++) {
 		// 	output("Element: " + strArr[i]);
@@ -35,58 +35,58 @@ public class App {
 		// Mehrdimensionale Arrays
 		// String [][] strMdArr = new String [2][2];
 		
-		// String [][] strMdArr = {{"Max","Mustermann"},{"Maxine","Musterfrau"}};
+		String [][] strMdArr = {{"Max","Mustermann"},{"Maxine","Musterfrau"}};
 		
 		// {{0,2,1},{1,0,0}}  x|y|z
 		
-		// output(strMdArr[0][0]);
-		// output(strMdArr[1][1]);
+		output(strMdArr[0][0]);
+		output(strMdArr[1][1]);
 		
-		// // nested loops
-		// for (int i = 0; i < strMdArr.length; i++) {
-		// 	for (int j = 0; j < strMdArr.length; j++) {
-		// 		output("Indizes i/j: " + "i:" +  i + " j:"+ j + " " + strMdArr[i][j]);
-		// 	}
-		// }
+		// nested loops
+		for (int i = 0; i < strMdArr.length; i++) {
+			for (int j = 0; j < strMdArr.length; j++) {
+				output("Indizes i/j: " + "i:" +  i + " j:"+ j + " " + strMdArr[i][j]);
+			}
+		}
 		
 		// // Deklaration | Chars
-		// char [] letters = {'d','c','a','A','b'};
+		char [] letters = {'d','c','a','A','b'};
 	
-		// 	output("");
-		// 	output("---- vor Sortierung: ");
-		// 	for (int i = 0; i < letters.length; i++) {
-		// 		output(letters[i]);	
-		// 	}
+			output("");
+			output("---- vor Sortierung: ");
+			for (int i = 0; i < letters.length; i++) {
+				output(letters[i]);	
+			}
 
 		// // Sortierfkt. --> sortiertes Array
-		// Arrays.sort(letters);  // Statische Klasse | Methode
+		Arrays.sort(letters);  // Statische Klasse | Methode
 
-		// output("");
-		// output("---- nach Sortierung: ");
-		// for (int i = 0; i < letters.length; i++) {
-		// 	output(letters[i]);	
-		// }
+		output("");
+		output("---- nach Sortierung: ");
+		for (int i = 0; i < letters.length; i++) {
+			output(letters[i]);	
+		}
 
-		// 	output("");
-		// 	output("---- Suche: erfolgreich ----");
-		// 	// ... danach Suche (1)
-		// 	int searchIndex = Arrays.binarySearch(letters,'a');
-		// 	output("");
-		// 	output("---- Binäre Suche: ");
-		// 	output("searchIndex: " + searchIndex);
+			output("");
+			output("---- Suche: erfolgreich ----");
+			// ... danach Suche (1)
+			int searchIndex = Arrays.binarySearch(letters,'a');
+			output("");
+			output("---- Binäre Suche: ");
+			output("searchIndex: " + searchIndex);
 		
-		// 	// output("---- Suche: erfolglos ----");
-		// 	// ... danach Suche (2)
-		// 	searchIndex = Arrays.binarySearch(letters,'e');
-		// 	output("searchIndex: " + searchIndex);  // -(length+1)
+			// output("---- Suche: erfolglos ----");
+			// ... danach Suche (2)
+			searchIndex = Arrays.binarySearch(letters,'e');
+			output("searchIndex: " + searchIndex);  // -(length+1)
 		// 	// .... isInArray()
 			
 		// 	output("---- fill() ----");
 		// 	//.. Array füllen
-		// 	Arrays.fill(letters, '0');
-		// 	for (int i = 0; i < letters.length; i++) {
-		// 		output((letters[i]));
-		// 	}
+			Arrays.fill(letters, '0');
+			for (int i = 0; i < letters.length; i++) {
+				output((letters[i]));
+			}
 
     }
 
